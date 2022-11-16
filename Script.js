c952b0369bbe87f9f69756de6c2a65eb866faddd
dongjinkimbig <kdjkdj11@naver.com>
2022-11-16 17:35:43 +0900

    JS CSS Index 수정

--- Script.js
+++ Script.js
@@ -16,22 +16,28 @@ document.addEventListener('scroll',() => {
     }
 });
 
-// 메뉴바 버튼 스무스처리
+// 메뉴바 버튼 처리
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(e)=>{
-    // console.log(e);
+
     const target = e.target;
     const link = target.dataset.link;
     if(link==null){
         return;
-    }
-    // console.log(link);
+    }    
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// 모바일 토글버트ㅡㄴ
+const navbarToggleBtn = document.querySelector('.navbar__togle-btn');
+navbarToggleBtn.addEventListener('click',()=>{
+    navbarMenu.classList.toggle('open');
 });
 
 // contact 버튼 스무스처리
 const contactBtn = document.querySelector('.home__contact');            // .빼먹냐 
-contactBtn.addEventListener('click',(e)=>{
+contactBtn.addEventListener('click',()=>{
     scrollIntoView('#contact');
 });
 
@@ -53,11 +59,95 @@ arrow.addEventListener('click',(e)=>{
     scrollIntoView('#home');
 });
 
-// 프로젝트 버튼 처리
-
-
+// 프로젝트 버튼 처리 (선택한거 보이기)
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click',(e)=>{
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    const active = document.querySelector('.category__btn.selected');
+    if(active != null){
+        active.classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+    projectContainer.classList.add('anim-out');
+    setTimeout(()=>{
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter === "*" || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    },300)
+});
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:'smooth'});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact'
+];
+
+const sections = sectionIds.map((id)=> document.querySelector(id));
+// console.log(sections);
+const navItems = sectionIds.map((id)=> document.querySelector(`[data-link="${id}"]`));
+// console.log(navItems);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions ={
+    root:null,
+    rootmargin:'0px',
+    threshold:0.3
+}
+
+const observerCallback = (entries,observer) => {
+    entries.forEach((entry)=>{
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            console.log('y');
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            // console.log(index);
+            if(entry.boundingClientRect.y <0){
+                selectedNavIndex = index +1;
+            }else{
+                selectedNavIndex = index -1;
+            }
+            console.log(selectedNavIndex);
+        }
+    });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel',()=>{
+    if(window.scrollY === 0){
+        selectedNavIndex = 0;
+    }else if(
+        window.scrollY + window.innerHeight === document.body.clientHeight
+    ){
+        selectedNavIndex = navItems.length -1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
